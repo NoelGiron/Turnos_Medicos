a89890ca9258c30520d58f4b6cbc35bff63609fb
NoelGiron <mngiron@hotmail.com>
2025-09-03 16:10:40 -0600

    se creo la ventana principal en el Main con la libreria tkinter

---
+++ Main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+#Ventana principal
+ventana = tk.Tk()
+ventana.title("Ventana de prueba")
+ventana.geometry("800x600")
+
+
+
+if __name__=="__main__":
+    ventana.mainloop()
