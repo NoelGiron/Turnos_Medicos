35811c23020d6267feb556332857c21e8d1665cd
NoelGiron <mngiron@hotmail.com>
2025-09-03 16:29:53 -0600

    se creo una lista de las opciones medicas del paciente

--- Main.py
+++ Main.py
@@ -5,6 +5,21 @@ ventana = tk.Tk()
 ventana.title("Ventana de prueba")
 ventana.geometry("800x600")
 
+#paciente
+etiqueta_nombre = tk.Label(ventana, text="Nombre del paciente")
+etiqueta_nombre.pack(pady=5)
+nombre_paciente = tk.Entry(ventana)
+nombre_paciente.pack(pady=5)
+
+etiqueta_especialidad = tk.Label(ventana, text="Especialidad")
+etiqueta_especialidad.pack(pady=5)
+lista_especialidad = tk.Listbox(ventana, height=4)
+lista_especialidad.pack(pady=5)
+lista_especialidad.insert(1, "Medicina General")
+lista_especialidad.insert(2, "Pediatría")
+lista_especialidad.insert(3, "Ginecología")
+lista_especialidad.insert(4, "Dermatología")
+
 
 
 if __name__=="__main__":
