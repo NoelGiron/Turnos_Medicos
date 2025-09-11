4b6f707ee7866be224d4b3889c4f4b57b752e8d4
NoelGiron <mngiron@hotmail.com>
2025-09-11 14:11:04 -0600

    se modifico el metodo graficar para que escribiera un .dot

--- Cola.py
+++ Cola.py
@@ -1,4 +1,5 @@
 from graphviz import Digraph
+import os
 class Cola:
     def __init__(self):
         self.elementos = []
@@ -26,29 +27,61 @@ class Cola:
         return self.elementos.copy()
     
     def graficar_lista(self):
-        dot = Digraph("lista de pacientes", format='png')
-        dot.attr(rankdir='LB')
+        # dot = Digraph("lista de pacientes", format='png')
+        # dot.attr(rankdir='LB')
 
-        tabla_html = '''<<TABLE BORDER="1" CELLBORDER="1" CELLSPACING="0">
-        <TR>
-            <TD BGCOLOR="#4CAF50"><B>Nombre</B></TD>
-            <TD BGCOLOR="#4CAF50"><B>Edad</B></TD>
-            <TD BGCOLOR="#4CAF50"><B>Especialidad</B></TD>
-        </TR>'''
+        # tabla_html = '''<<TABLE BORDER="1" CELLBORDER="1" CELLSPACING="0">
+        # <TR>
+        #     <TD BGCOLOR="#4CAF50"><B>Nombre</B></TD>
+        #     <TD BGCOLOR="#4CAF50"><B>Edad</B></TD>
+        #     <TD BGCOLOR="#4CAF50"><B>Especialidad</B></TD>
+        # </TR>'''
     
-        for paciente in self.elementos:
-            tabla_html += f'''
-            <TR>
-                <TD>{paciente.nombre}</TD>
-                <TD>{paciente.edad} años</TD>
-                <TD>{paciente.especialidad}</TD>
-            </TR>'''
+        # for paciente in self.elementos:
+        #     tabla_html += f'''
+        #     <TR>
+        #         <TD>{paciente.nombre}</TD>
+        #         <TD>{paciente.edad} años</TD>
+        #         <TD>{paciente.especialidad}</TD>
+        #     </TR>'''
             
-        tabla_html += '''
-            </TABLE>>'''
+        # tabla_html += '''
+        #     </TABLE>>'''
         
-        dot.node('tabla', tabla_html, shape='none')
-        dot.render('tabla_pacientes', view=True)
+        # dot.node('tabla', tabla_html, shape='none')
+        # dot.render('tabla_pacientes', view=True)
 
+        contenido = """
+            graph G{
+                node [shape=plaintext]; 
+                tablero[label=<<TABLE BORDER="1" CELLBORDER="1" CELLSPACING="0">
+                    <TR>
+                        <TD BGCOLOR="#4CAF50"><B>Nombre</B></TD>
+                        <TD BGCOLOR="#4CAF50"><B>Edad</B></TD>
+                        <TD BGCOLOR="#4CAF50"><B>Especialidad</B></TD>
+                    </TR>
+                    """
+        
+        tabla_html = ""
+        
+        for paciente in self.elementos:
+             tabla_html += f'''
+             <TR>
+                 <TD>{paciente.nombre}</TD>
+                 <TD>{paciente.edad} años</TD>
+                 <TD>{paciente.especialidad}</TD>
+             </TR>
+             '''
+
+        contenido += tabla_html
+
+        contenido += """
+            </TABLE>>];
+            }
+            """
+        file = open("Grafica.dot", "w", encoding="utf-8")
+        file.write(contenido)
+        file.close()
+        os.system("dot -Tpng Grafica.dot -o Grafica.png")
 
     
